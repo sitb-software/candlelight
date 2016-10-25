21e6581b398cc46e161e3fcabc66b0b2f750ceec
Sean <sean.snow@live.com>
2016-10-25 16:09:13 +0800

    解析body参数

---
+++ middleware/findBodyParams.ts
@@ -0,0 +1,34 @@
+import Context from '../http/Context';
+import * as contentTypeUtils from 'content-type';
+import { parse } from 'url';
+
+const getRawBody = require('raw-body');
+
+/**
+ * 解析请求中的表单参数
+ * @author 田尘殇Sean(sean.snow@live.com)
+ * @date 2016/10/25
+ */
+export default (ctx: Context, next) => {
+  const req = ctx.request;
+  const contentType = contentTypeUtils.parse(req);
+
+  getRawBody(req, {
+    length: req.headers['content-length'],
+    encoding: contentType.parameters.charset || 'utf-8'
+  }, (err, res)=> {
+    if (err) {
+      console.error('解析body失败', err);
+    }
+    if (res) {
+      let body = null;
+      if (contentType.type === 'application/x-www-form-urlencoded') {
+        body = parse(`/?${res}`, true).query;
+      } else if (contentType.type.includes('json')) {
+        body = JSON.parse(res);
+      }
+      ctx.body = body;
+    }
+    next();
+  });
+};
