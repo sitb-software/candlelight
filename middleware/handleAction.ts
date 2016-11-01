50940e1ae5514d902bb1d1cb0db383e751c9197d
Sean <sean.snow@live.com>
2016-11-01 14:20:06 +0800

    处理异步action

--- middleware/handleAction.ts
+++ middleware/handleAction.ts
@@ -1,14 +1,33 @@
 import Context from '../http/Context';
+
+function response({context, result}) {
+  if (context.route.isRestful) {
+    context.response.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    context.response.end(typeof result === 'string' ? result : JSON.stringify(result));
+  }
+}
+
 /**
  * @author 田尘殇Sean(sean.snow@live.com)
  * @date 16/9/7
  */
 export default (ctx: Context) => {
-  const result = ctx.route.handler(ctx);
-  if (ctx.route.isRestful) {
-    ctx.response.writeHead(200, {
-      'Content-Type': 'application/json'
+  const res = ctx.route.handler(ctx);
+  if (res instanceof Promise) {
+    res.then(result => {
+      response({
+        context: ctx,
+        result: result
+      });
+    }, error => {
+      console.error(error);
+    })
+  } else {
+    response({
+      context: ctx,
+      result: res
     });
-    ctx.response.end(typeof result === 'string' ? result : JSON.stringify(result));
   }
 }
