4c9afbe78f744b598d23891ab13eac5318b03447
Sean <sean.snow@live.com>
2017-11-15 14:16:19 +0800

    template

--- http/handleRequest.ts
+++ http/handleRequest.ts
@@ -1,11 +1,12 @@
 import Context from "./Context";
+
 /**
  * 处理请求
  * @author 田尘殇Sean(sean.snow@live.com)
  * @date 16/9/6
  */
-export default ({request, response, route, middlewareStack}) => {
-  let ctx:Context = {request, response};
+export default ({request, response, route, template, middlewareStack}) => {
+  let ctx: Context = {request, response, template};
   ctx.route = route;
   const next = function () {
     let middleware = middlewareStack.shift();
