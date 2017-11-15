4348bf17c36dff41ef279796d7102e55a773b3d1
Sean <sean.snow@live.com>
2017-11-15 14:09:24 +0800

    mapping

--- annotation/RequestMapping.ts
+++ annotation/RequestMapping.ts
@@ -6,7 +6,7 @@ import HttpMethod from '../http/HttpMethod';
  * @author 田尘殇Sean(sean.snow@live.com)
  * @date 16/9/6
  */
-export default ({path, method = HttpMethod.GET}): Function => {
+export default function RequestMapping({path, method = HttpMethod.GET}): Function {
   return function (target, propertyKey: string, descriptor: TypedPropertyDescriptor<any>): TypedPropertyDescriptor<any> {
     if (!target.handlers) {
       target.handlers = {};
@@ -25,3 +25,19 @@ export default ({path, method = HttpMethod.GET}): Function => {
     return descriptor;
   };
 }
+
+
+/**
+ * get request
+ * @param {string} path 路径
+ * @constructor
+ */
+export const GetMapping = (path = '') => RequestMapping({path});
+
+export const PostMapping = (path = '') => RequestMapping({path, method: HttpMethod.POST});
+
+export const PutMapping = (path = '') => RequestMapping({path, method: HttpMethod.PUT});
+
+export const PatchMapping = (path = '') => RequestMapping({path, method: HttpMethod.PATCH});
+
+export const DeleteMapping = (path = '') => RequestMapping({path, method: HttpMethod.DELETE});
