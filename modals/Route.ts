70447d1dd1da0d94193b36ebf374710907e1d3a8
Sean <sean.snow@live.com>
2017-11-17 20:10:27 +0800

    parameterTypes

--- modals/Route.ts
+++ modals/Route.ts
@@ -1,10 +1,12 @@
 import HttpMethod from "../http/HttpMethod";
+import ParameterType from "./ParameterType";
 
 interface Route {
-  uri?:string,
-  method: HttpMethod,
+  uri?: string
+  method: HttpMethod
   handler?: Function
   isRestful: boolean
+  parameterTypes: { [key: string]: ParameterType }
 }
 
 export default Route;
