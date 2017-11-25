aab96d6e7a8e98bbbf32ea05d79b2c2cf37095e0
Sean <sean.snow@live.com>
2017-11-25 14:28:01 +0800

    参数验证数据类型

---
+++ modals/ParamsValid.ts
@@ -0,0 +1,46 @@
+import { Range } from './Range';
+/**
+ * @author Sean(sean.snow@live.com) createAt 17-11-17
+ */
+
+
+/**
+ * 验证支持的数据类型
+ */
+export enum Type {
+  string,
+  number,
+  boolean,
+  url,
+  email,
+  tel,
+  enumerable,
+  object,
+  array
+}
+
+
+/**
+ * @param value 当前值
+ * @param data 所有数据
+ */
+export type Validator = (value, source, rule: Rule) => boolean;
+
+export interface Rule {
+  key: string
+  type?: Type
+  required?: boolean
+  pattern?: RegExp
+  range?: Range
+  min?: number
+  max?: number
+  length?: number
+  validator?: Validator
+  /**
+   * 如果type为object或者array,可以使用该字段配置其中的值
+   */
+  fields?: Array<Rule>
+  values?: Array<any>
+  message?: string,
+  messages?: Array<string>
+}
