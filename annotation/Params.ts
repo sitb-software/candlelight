6f8d64a0e684b099c17382c79570342c8f557d7a
Sean <sean.snow@live.com>
2017-11-25 14:24:56 +0800

    Valid 表单验证

--- annotation/Params.ts
+++ annotation/Params.ts
@@ -2,8 +2,10 @@
  * @author Sean(sean.snow@live.com) createAt 17-11-17
  */
 import { ParameterFrom } from "../modals/ParameterType";
+import { Rule } from '../modals/ParamsValid';
+import validator from '../validator';
 
-function check(target: any, propertyKey: string, descriptor: number) {
+function check(target: any, propertyKey: string, descriptor: number, data) {
   if (!target.handlers) {
     target.handlers = {};
   }
@@ -13,6 +15,13 @@ function check(target: any, propertyKey: string, descriptor: number) {
   if (!target.handlers[propertyKey].parameterTypes) {
     target.handlers[propertyKey].parameterTypes = {};
   }
+  if (!target.handlers[propertyKey].parameterTypes[descriptor]) {
+    target.handlers[propertyKey].parameterTypes[descriptor] = {};
+  }
+  target.handlers[propertyKey].parameterTypes[descriptor] = {
+    ...target.handlers[propertyKey].parameterTypes[descriptor],
+    ...data
+  }
 }
 
 /**
@@ -20,24 +29,30 @@ function check(target: any, propertyKey: string, descriptor: number) {
  * @author Sean(sean.snow@live.com) createAt 17-11-17
  */
 export const RequestBody = (target: any, propertyKey: string, descriptor: number) => {
-  check(target, propertyKey, descriptor);
-  target.handlers[propertyKey].parameterTypes[descriptor] = {
+  check(target, propertyKey, descriptor, {
     type: ParameterFrom.BODY
-  };
+  });
 };
 
 export const RequestParam = (key: string, required: boolean = true) => (target: any, propertyKey: string, descriptor: number) => {
-  check(target, propertyKey, descriptor);
-  target.handlers[propertyKey].parameterTypes[descriptor] = {
+  check(target, propertyKey, descriptor, {
     key,
     required
-  };
+  });
 };
 
 export const PathVariable = (key: string) => (target: any, propertyKey: string, descriptor: number) => {
-  check(target, propertyKey, descriptor);
-  target.handlers[propertyKey].parameterTypes[descriptor] = {
+  check(target, propertyKey, descriptor, {
     type: ParameterFrom.PATH,
     key
-  };
+  });
 };
+
+
+export function Valid(rules: Array<Rule>) {
+  return (target, propertyKey: string, descriptor: number) => {
+    check(target, propertyKey, descriptor, {
+      valid: (source) => validator(source, rules)
+    });
+  }
+}
